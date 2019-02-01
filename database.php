797b196e390bb09790a29b8f0bf2b2aefca487e0
Tyrell D. Wheeler <tdwheele@csis.svsu.edu>
2019-01-31 19:01:38 -0500

    new data

--- database.php
+++ database.php
@@ -4,7 +4,7 @@ class Database
 	// these are not the real passwords
 	// for real passwords, see file in ../database subdirectory
 	private static $dbName = 'tdwheele355wi19_northwind' ; 
-	private static $dbHost = 'localhost' ;
+	private static $dbHost = '10.8.30.49' ;
 	private static $dbUsername = 'tdwheele355wi19';
 	private static $dbUserPassword = 'myAdmin247';
 	
